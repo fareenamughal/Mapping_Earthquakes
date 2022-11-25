37ac74dd788cef55f29cbb38e6653eeca6a9324b
fareenamughal <fareena.mughal@utoronto.ca>
2022-11-25 01:03:09 -0500

    main

---
+++ Simple_Map/Static/js/config.js
@@ -0,0 +1,7 @@
+//<!--
+  //  config.js. This file will hold our Mapbox API key.
+  //  The config.js file is like the config.pyfile we used to hold the OpenWeatherMap API and Google API keys.
+//-->
+
+// API key
+const API_KEY = "pk.eyJ1IjoiZmFyZWVuYW11Z2hhbCIsImEiOiJjbDhib2dsZmIwOTR6M3BuOXMyZjdnZXpuIn0.mzxFsw8HF9qEj2o6SZuTyA";
