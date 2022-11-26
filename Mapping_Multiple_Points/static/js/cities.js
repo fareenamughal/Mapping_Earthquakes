9ea10db5b67327cc91b702aa01da4da2d037014b
fareenamughal <fareena.mughal@utoronto.ca>
2022-11-26 17:13:03 -0500

    Mapping_Multiple_Points

--- Mapping_Multiple_Points/static/js/cities.js
+++ Mapping_Multiple_Points/static/js/cities.js
@@ -31,8 +31,3 @@ let cities = [{
   }
   ];
 
-// Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-    console.log(city)
-    L.marker(city.location).addTo(map);
-});
